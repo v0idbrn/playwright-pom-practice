a4e285e27426b26f950bf5d9609398a74900658d
v0idbrn <sasukiikneel14@gmail.com>
2026-03-20 20:26:18 -0300

    Add negative login test case

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,13 +1,22 @@
 from pages.login_page import LoginPage
 import pytest
 
+# TEST 1: Login success
 def test_login_success(page):
-    # 1. Instanciamos la página
     login = LoginPage(page)
-    
-    # 2. Ejecutamos las acciones
     login.navigate()
     login.login("standard_user", "secret_sauce")
-    
-    # 3. Verificamos que todo esté OK
     assert page.url == "https://www.saucedemo.com/inventory.html"
+
+# TEST 2: Login failure
+def test_login_failure(page):
+    login = LoginPage(page)
+    login.navigate()
+    
+    # Mandamos cualquier fruta en el pass
+    login.login("standard_user", "password_incorrecta_hdp")
+    
+    # Verificamos que aparezca el mensaje de error
+    # Usamos el locator que ya definimos en la clase LoginPage
+    assert login.error_message.is_visible()
+    assert "Username and password do not match" in login.error_message.text_content()
